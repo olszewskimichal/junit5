b3cd67425ee85252dc2ec1c7e5d043f7e952cc4e
Michał <olszewskimichal@outlook.com>
2017-11-14 18:14:21 +0100

    lifecycle test

---
+++ src/test/java/lifecycle/LifecycleTest.java
@@ -0,0 +1,25 @@
+package lifecycle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class LifecycleTest implements TestLifecycleLogger, TimeExecutionLogger {
+
+  @Test
+  void sleep20ms() throws Exception {
+    Thread.sleep(20);
+  }
+
+  @Test
+  void sleep50ms() throws Exception {
+    Thread.sleep(50);
+  }
+
+  @Test
+  void isEqualValue() {
+    assertEquals(1, 1, "is always equal");
+  }
+
+}
---
+++ src/test/java/lifecycle/TimeExecutionLogger.java
@@ -0,0 +1,11 @@
+package lifecycle;
+
+import extensions.TimingExtension;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Tag("timed")
+@ExtendWith(TimingExtension.class)
+public interface TimeExecutionLogger {
+
+}
