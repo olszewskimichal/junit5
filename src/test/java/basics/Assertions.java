09ff627607f3927e10b45fd63753a46ae37469b6
Michał <olszewskimichal@outlook.com>
2017-11-14 17:16:13 +0100

    asercje zagniezdzone

--- src/test/java/basics/Assertions.java
+++ src/test/java/basics/Assertions.java
@@ -2,9 +2,11 @@ package basics;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
+import lombok.Value;
 import org.junit.jupiter.api.Test;
 
 public class Assertions {
@@ -25,5 +27,40 @@ public class Assertions {
         () -> assertEquals(numbers[4], 4)
     );
   }
+
+  @Test
+  void dependentAssertions() {
+    Person person = new Person("Jan", "De");
+
+    assertAll("properties",
+        () -> {
+          String firstName = person.getFirstName();
+          assertNotNull(firstName);
+
+          assertAll("first name",
+              () -> assertTrue(firstName.startsWith("J")),
+              () -> assertTrue(firstName.endsWith("n"))
+          );
+        },
+        () -> {
+          String lastName = person.getLastName();
+          assertNotNull(lastName);
+
+          // Executed only if the previous assertion is valid.
+          assertAll("last name",
+              () -> assertTrue(lastName.startsWith("D")),
+              () -> assertTrue(lastName.endsWith("e"))
+          );
+        }
+    );
+  }
+
+  @Value
+  private class Person {
+
+    private final String firstName;
+    private final String lastName;
+  }
+
 
 }
