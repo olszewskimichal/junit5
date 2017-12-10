9324db698cb738ccd006e7ffac1c0c61c20a48e8
Michał <olszewskimichal@outlook.com>
2017-12-10 11:54:00 +0100

    refaktoryzacja

--- src/test/java/injections/DependencyInjectionTest.java
+++ src/test/java/injections/DependencyInjectionTest.java
@@ -1,15 +1,22 @@
 package injections;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import extensions.MockitoExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestReporter;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 
-public class DependencyInjectionTest {
+class DependencyInjectionTest {
 
   @BeforeEach
   void init(TestInfo testInfo) {
@@ -35,5 +42,20 @@ public class DependencyInjectionTest {
   @RepeatedTest(2)
   void test(RepetitionInfo repetitionInfo) {
     System.out.println("** Test " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+  }
+
+  @Nested
+  @ExtendWith(MockitoExtension.class)
+  class MockitoInjectionTest {
+
+    @BeforeEach
+    void init(@Mock PersonInterface person) {
+      when(person.getFirstName()).thenReturn("Dilbert");
+    }
+
+    @Test
+    void simpleTestWithInjectedMock(@Mock PersonInterface person) {
+      assertEquals("Dilbert", person.getFirstName());
+    }
   }
 }
--- src/test/java/injections/PersonInterface.java
+++ src/test/java/injections/PersonInterface.java
@@ -1,4 +1,4 @@
-package extensions;
+package injections;
 
 public interface PersonInterface {
 
